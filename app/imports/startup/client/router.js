d784741818abb53b80354217a5449bb7c625046a
Kurt Nikaitani <knikaitani@gmail.com>
2017-03-22 17:38:28 -1000

    update

--- app/imports/startup/client/router.js
+++ app/imports/startup/client/router.js
@@ -2,9 +2,16 @@ import { FlowRouter } from 'meteor/kadira:flow-router';
 import { BlazeLayout } from 'meteor/kadira:blaze-layout';
 
 FlowRouter.route('/', {
+  name: 'Landing_Page',
+  action() {
+    BlazeLayout.render('App_Body', { main: 'Landing_Page' });
+  },
+});
+
+FlowRouter.route('/home-page', {
   name: 'Home_Page',
   action() {
-    BlazeLayout.render('App_Body', { main: 'Home_Page' });
+    BlazeLayout.render('App_Body', { main: 'Homw_Page' });
   },
 });
 
---
+++ app/imports/ui/pages/landing-page.js
@@ -0,0 +1,3 @@
+/**
+ * Created by kurtnikaitani on 3/22/17.
+ */
